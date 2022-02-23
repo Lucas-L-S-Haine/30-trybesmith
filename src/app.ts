548b42ed496a8ccc2c5a0fb570c14696ce83dd43
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-02-23 15:57:36 -0300

    Adiciona middlewares e rota padrão.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
-import express from 'express';
+import express, { json, Request, Response } from 'express';
+import user from './controllers/userController';
+import errorHandler from './middlewares/errorMiddleware';
+import notFoundHandler from './middlewares/notFoundMiddleware';
 
 const app = express();
 
-app.use(express.json());
+app.get('/', (_req: Request, res: Response) => (res.status(200).send('Online!')));
+
+app.use(json());
+app.use('/users', user);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 export default app;
