651c39cf01d4f23ae430b8ca8a239a284ba0e5af
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-02-23 23:45:43 -0300

    Função faz uma ponte entre camada de controllers e models. Usuário criado com sucesso. Validação ainda falta ser implementada.

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,12 @@
 import User from '../models/user';
+import { UserI } from '../interfaces';
+import { createToken } from '../auth';
 
-const createOne = async (user: [string, string, number, string]) => {
-  await User.create(user);
+const createOne = async (user: UserI) => {
+  const token = createToken(user);
+  const { username, classe, level, password } = user;
+  await User.create([username, classe, level, password]);
+  return token;
 };
 
 export default {
