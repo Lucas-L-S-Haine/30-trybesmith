b1fcdee4b3d9ff8c1ad1cda54dbcd6bf599a7fbe
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-02-23 23:44:47 -0300

    Arquivo com métodos relacionados ao JWT.

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,24 +2,11 @@ import { Response, NextFunction } from 'express';
 import { sign, decode, verify } from 'jsonwebtoken';
 import HttpException from '../classes/httpException';
 import UserRequest from '../classes/userRequest';
+import { UserI } from '../interfaces';
 
 const JWT_SECRET = 'SA9D8JASD98HYAF9SFN89ash008yh08H808yh9SC8GH9ASCA987C';
 
-export function newToken(user: {
-  username: string,
-  classe: string,
-  level: number,
-  password: string,
-}) {
-  return sign(user, JWT_SECRET);
-}
-
-export function readToken(token: string) {
-  return decode(token);
-}
-
-export function validateToken(req: UserRequest, _res: Response, next: NextFunction) {
-  const token: string = req.headers.authorization;
+const verifyTokenPresence = (token: string): void => {
   if (!token) {
     const error: HttpException = {
       name: 'authentication error',
@@ -29,11 +16,34 @@ export function validateToken(req: UserRequest, _res: Response, next: NextFuncti
     };
     throw error;
   }
+};
+
+const verifyTokenValidity = (token: string): string => {
   try {
-    verify(token, JWT_SECRET);
+    return `${verify(token, JWT_SECRET)}`;
   } catch (err) {
-    const payload = verify(token, JWT_SECRET);
-    req.user = `${payload}`;
-    next();
+    const error: HttpException = {
+      name: 'authentication error',
+      status: 401,
+      message: 'Expired or invalid token',
+      error: 'unauthorized',
+    };
+    throw error;
   }
+};
+
+export function createToken(user: UserI) {
+  return sign(user, JWT_SECRET);
+}
+
+export function readToken(token: string) {
+  return decode(token);
+}
+
+export function validateToken(req: UserRequest, _res: Response, next: NextFunction) {
+  const token: string = req.headers.authorization;
+  verifyTokenPresence(token);
+  const payload = verifyTokenValidity(token);
+  req.user = payload;
+  next();
 }
