ec12ab4ffea79558c4ec5d3f8fc026d9b52609d3
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-02-26 20:37:52 -0300

    Substitui tipo Request pela interface RequestI.

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 import service from '../services/productService';
-import { ProductI } from '../interfaces';
+import { ProductI, RequestI } from '../interfaces';
 
-export const createOne = async (req: Request, res: Response, next: NextFunction) => {
+export const createOne = async (req: RequestI, res: Response, next: NextFunction) => {
   try {
     const product: ProductI = req.body;
     const token = await service.createOne(product);
@@ -12,7 +12,7 @@ export const createOne = async (req: Request, res: Response, next: NextFunction)
   }
 };
 
-export const readAll = (req: Request, res: Response, next: NextFunction) => service
+export const readAll = (req: RequestI, res: Response, next: NextFunction) => service
   .readAll()
   .then((products: ProductI[]) => res.status(200).json(products))
   .catch((err: unknown) => next(err));
