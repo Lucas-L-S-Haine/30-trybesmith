e7dacc1c885d27e72a4fbb096778eada05ffc470
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-02-26 21:02:19 -0300

    Arquivos com classes não utilizadas removidos.

--- src/classes/headers.ts
+++
@@ -1,9 +0,0 @@
-export default class UserHeaders extends Headers {
-  authorization?: string;
-
-  constructor(authorization: string) {
-    super();
-
-    this.authorization = authorization;
-  }
-}
