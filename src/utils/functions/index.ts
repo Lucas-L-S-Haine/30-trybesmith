f3b630230fdd144dbdb9df2430d92ce74534fa6c
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-02-26 21:32:00 -0300

    Nova função para validar formato da string amount por expressões regulares.

--- src/utils/functions/index.ts
+++ src/utils/functions/index.ts
@@ -4,10 +4,12 @@ const isValidUsername = (username: string): boolean => regex.username.test(usern
 const isValidClass = (classe: string): boolean => regex.classe.test(classe);
 const isValidPassword = (password: string): boolean => regex.password.test(password);
 const isValidName = (name: string): boolean => regex.name.test(name);
+const isValidAmount = (name: string): boolean => regex.amount.test(name);
 
 export {
   isValidUsername,
   isValidClass,
   isValidPassword,
   isValidName,
+  isValidAmount,
 };
--- src/utils/regex/index.ts
+++ src/utils/regex/index.ts
@@ -2,10 +2,12 @@ const username = /^\S{3,}$/;
 const classe = /^\S{3,}$/;
 const password = /^\S{8,}$/;
 const name = /^\S{3,}$/;
+const amount = /^\S{3,}$/;
 
 export {
   username,
   classe,
   password,
   name,
+  amount,
 };
