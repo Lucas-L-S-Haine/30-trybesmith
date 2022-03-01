cd7876792ae13530af567bbf8848ab10492828b4
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-01 20:49:17 -0300

    Novo método update.

--- src/models/product.ts
+++ src/models/product.ts
@@ -5,19 +5,23 @@ const create = (product: [string, string]): Promise<void> => connection
   .execute('INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);', product)
   .then();
 
-const findAll = () => connection
+const findAll = (): Promise<ProductI[]> => connection
   .execute('SELECT * FROM Trybesmith.Products;')
   .then(([products]) => products as ProductI[]);
 
-const findOne = (name: [string]) => connection
+const findOne = (name: [string]): Promise<ProductI> => connection
   .execute('SELECT id, name, amount FROM Trybesmith.Products WHERE name = ?', name)
   .then(([data]) => data as ProductI[])
   .then(([product]) => product);
 
-const findByPk = (id: number) => connection
+const findByPk = (id: number): Promise<ProductI> => connection
   .execute('SELECT * FROM Trybesmith.Products WHERE id = ?', [id])
   .then(([data]) => data as ProductI[])
   .then(([product]) => product);
+
+const update = (orderId: number, productIds: number[]): Promise<void> => connection
+  .execute('UPDATE Trybesmith.Products SET orderId = ? WHERE id in (?);', [orderId, productIds])
+  .then();
 
 /*
 update
@@ -29,4 +33,5 @@ export default {
   findAll,
   findOne,
   findByPk,
+  update,
 };
