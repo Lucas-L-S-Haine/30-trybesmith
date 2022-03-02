ea0a814af201bea3c2aadebb7725ec2aea6e29a3
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-01 23:39:55 -0300

    Valor orderId adicionado à tabela de produtos.

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,11 +1,16 @@
 import Order from '../models/order';
+import Product from '../models/product';
 import { OrderI } from '../interfaces';
 import { orderValidate } from '../validations';
 
 const createOne = async (order: OrderI, userId: number) => {
   orderValidate(order);
+  const { products: productIds } = order;
   await Order.create([userId]);
-  return true;
+  const { id: orderId } = await Order.findLast() || 0;
+  await Product.update(orderId as number, productIds);
+  const productList = await Product.findMany(productIds);
+  return productList;
 };
 
 const readAll = async () => {
