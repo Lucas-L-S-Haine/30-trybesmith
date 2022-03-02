a3f202cf83146c163250912776ed42bdc1b6591b
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-01 21:27:05 -0300

    Mudança nas expressões regulares. Espaços vazios aceitos.

--- src/utils/regex/index.ts
+++ src/utils/regex/index.ts
@@ -1,8 +1,8 @@
-const username = /^\S{3,}$/;
-const classe = /^\S{3,}$/;
-const password = /^\S{8,}$/;
-const name = /^\S{3,}$/;
-const amount = /^\S{3,}$/;
+const username = /^[ ,\S]{3,}$/;
+const classe = /^[ ,\S]{3,}$/;
+const password = /^[ ,\S]{8,}$/;
+const name = /^[ ,\S]{3,}$/;
+const amount = /^[ ,\S]{3,}$/;
 
 export {
   username,
